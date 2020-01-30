6c0b84378c235801a6c9e90035845e99ac97d708
cMeTaHKo <bodia122@gmail.com>
2020-01-30 12:38:36 +0100

    first update with some logic

--- script.js
+++ script.js
@@ -1,78 +1,40 @@
 'use strict';
 
-let money,
-    deposit = confirm('У Вас есть депозит в банке?'),
-    income = 'freelance',
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', ''),
-    mission = 100000,
-    period = 12,
-    amount = 0,
-    expenses = [];
-
-let start = function() {
-    do { money = prompt('Ваш месячный доход?', '');
-    } while (isNaN(money) || money === '' || money === null);
-};
-start();
-
-let isNumber = function (n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-};
-
-let getExpensesMonth = function() { 
-    for (let i = 0; i < 2; i++ ) {
-        expenses[i] = prompt('Введите обязательную статью расходов?', '');
-
-        let count;
-        do {
-            count = prompt('Во сколько это обойдётся?', '');
-        } while (!isNumber(count));
-
-        amount += +count;
-    }
-    console.log('Обзятельные статьи расходов в месяц: ' + expenses);
-    return amount;
-};
-
-let expensesAmount = getExpensesMonth();
-console.log('Расходы за месяц: ' + expensesAmount);
-
-let getAccumulatedMonth = function() { return +money - expensesAmount; };
-
-let accumulatedMonth = getAccumulatedMonth(),
-budgetDay = +accumulatedMonth/30;
-
-let getTargetMonth = function() {
-    return Math.ceil(mission / accumulatedMonth); 
-};
-
-if(getTargetMonth() < 0) {
-    console.log('Цель не будет достигнута');
-} else {
-    console.log('Цель будет достигнута через ' + getTargetMonth() + ' месяцев');
+let min = 1,
+    max = 100;
+function randomAmount() {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
+randomAmount(min, max);
 
-console.log('Возможные расходы за ' + period + ' месяцев: ' + addExpenses.split(', '));
-console.log('Ваш дневной бюджет: ' + Math.floor(budgetDay));
+let userAmount = [+prompt('Угадай число от 1 до 100')];
+console.log(typeof(userAmount));
 
-function getStatusIncome() {
-    if (budgetDay >= 1200) {
-        console.log('У вас высокий уровень дохода');
-    } else if (budgetDay >= 600 && budgetDay < 1200) {
-        console.log('У вас средний уровень дохода');
-    } else if (budgetDay < 600 && budgetDay >= 1) {
-        console.log('К сожалению у вас уровень дохода ниже среднего');
-    } else if (budgetDay <= 0) {
-        console.log('Что то пошло не так!');
+function checker () {
+    if (typeof(userAmount) === isNaN || typeof(userAmount) === 'string') {
+        userAmount = +prompt('Введи число!');
+        // здесь надо рекурсия на введите число
+        // это ещё одно повторение
+        // if (typeof(userAmount) !== 'number' || typeof(userAmount) === isNaN || typeof(userAmount) === 'string') {
+        //     userAmount = prompt('Введи число!');
+        // }
+    } else if (typeof(userAmount) === null) {
+        return false;
+    } else {
+        if (userAmount > randomAmount()) {
+            alert('Загаданное число больше');
+            // здесь надо рекурсия чтобы игра продолжалась пока не отменят
+            userAmount = +prompt('Угадай число от 1 до 100');
+        } else if (userAmount < randomAmount()) {
+            alert('Загаданное число меньше');
+            // здесь надо рекурсия чтобы игра продолжалась пока не отменят
+            userAmount = +prompt('Угадай число от 1 до 100');
+        }
     }
+    console.log(typeof(userAmount));
+    console.log(typeof(randomAmount()));
+    console.log(randomAmount());
 }
-getStatusIncome();
-
-let showTypeOf = function() {
-    console.log('Тип переменной money : ' + typeof(+money));
-    console.log('Тип переменной deposit : ' + typeof(deposit));
-    console.log('Тип переменной income : ' + typeof(income));
-    console.log('Тип переменной budgetDay : ' + typeof(budgetDay));
-    console.log('Тип переменной addExpenses : ' + typeof(addExpenses));
-};
-showTypeOf();
+checker();
