ee9ad3131219a7d67a894403b62a358321568961
cMeTaHKo <bodia122@gmail.com>
2020-02-01 13:22:28 +0100

    rouletteJS fix

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
 'use strict';
-let randomAmount = function(min, max) {
-    min = 1;
-    max = 100;
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-};
 
-let num = randomAmount();
 let userAmount =  +prompt('Угадай число от 1 до 100');
-
-let firstQuestion = function() {
-
-    if ( Number.isNaN(userAmount) === true) {
-        userAmount = +prompt('Введи число!');
+        
+let headFunction = function() {
     
-    } else if(userAmount === 0 || userAmount === null) {
-        alert('Игра прервана! Перезагрузите страницу для новой игры!');
-        return;
+    let randomAmount = function(min, max) {
+        min = 1;
+        max = 100;
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
+    let num = randomAmount();
 
-    } else if (Number.isNaN(userAmount) === false) {
-        if (userAmount > num) {
-            alert('Загаданное число больше');
-            userAmount =  +prompt('Угадай число от 1 до 100');
+    let question = function() {
 
-        } else if ( userAmount < num) {
-            alert('Загаданное число меньше');
-            userAmount =  +prompt('Угадай число от 1 до 100');
-
-        } else if (userAmount === num) {
-            alert("Поздравляю, вы угадали цифру! Перезагрузите страницу для новой игры!");
+        if ( Number.isNaN(userAmount) === true) {
+            userAmount = +prompt('Введи число!');
+        
+        } else if(userAmount === 0 || userAmount === null) {
+            alert('Игра прервана! Перезагрузите страницу для новой игры!');
             return;
+    
+        } else if (Number.isNaN(userAmount) === false) {
+            if (userAmount > num) {
+                alert('Загаданное число больше');
+                userAmount =  +prompt('Угадай число от 1 до 100');
+    
+            } else if ( userAmount < num) {
+                alert('Загаданное число меньше');
+                userAmount =  +prompt('Угадай число от 1 до 100');
+    
+            } else if (userAmount === num) {
+                alert("Поздравляю, вы угадали цифру! Перезагрузите страницу для новой игры!");
+                return;
+            }
         }
-    } else {
-        console.log('Что-то пошло не так!');
-    }
-    return firstQuestion();
+        return question();
+    };
+    console.log(num);
+    return question();
 };
-
-
 
 let closeFunc = function() {
-    // Вывожу цифру после первой попытки для тестов и вида что она не меняется.
-    console.log(num);
-
-    firstQuestion();
+    headFunction();
 }();
