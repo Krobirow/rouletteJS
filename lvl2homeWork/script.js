90141fb7cb52be0b8f7eee3e64a49c58b12dfa9b
cMeTaHKo <bodia122@gmail.com>
2020-02-01 20:38:25 +0100

    closed function upd

--- lvl2homeWork/script.js
+++ lvl2homeWork/script.js
@@ -7,23 +7,33 @@ const game = function() {
     const randomNum =  Math.floor(Math.random() * 100) + 1;
     console.log(randomNum);
 
-    return function question() {
-        let userAmount = prompt('Угадай число от 1 до 100');
+    return function question(count) {
+
+        function closingFunc() {
+            if(count > 1) {
+                count--;
+                question(count);
+            } else if (count <= 1) {
+                return;
+            }
+        }
+
+        let userAmount = prompt('Попробуй угадать число от 1 до 100 в рулетке! Количество попыток ограничено в: ' + count);
 
         if (isNum(userAmount)) {
             userAmount = +userAmount;
-            
+
             if (userAmount < 101 && userAmount > randomNum) {
-                alert('Загаданное число меньше');
-                question();
+                alert('Загаданное число меньше, осталось попыток: ' + count);
+
             } else if (userAmount < 101 && userAmount < randomNum) {
-                alert('Загаданное число больше');
-                question();
+                alert('Загаданное число больше, осталось попыток: ' + count);
+
             } else if (userAmount === randomNum) {
                 alert("Поздравляю, вы угадали цифру! Перезагрузите страницу для новой игры!");
             } else {
-                alert("Вы ввели число больше 100!");
-                question();
+                alert('Вы ввели число больше 100! Oсталось попыток: ' + count);
+                
             }
         } else if(userAmount === null) {
             alert('Вы прервали игру! Перезагрузите страницу для новой игры!');
@@ -31,8 +41,9 @@ const game = function() {
             alert('Введите число!');
             question();
         }
+        closingFunc();
     };
 };
 
 let playGame = game();
-playGame();
+playGame(10);
