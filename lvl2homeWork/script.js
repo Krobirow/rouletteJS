ac2fcbc4d122c5b1d15e08e59fdcf9d8d16a9f6b
cMeTaHKo <bodia122@gmail.com>
2020-02-01 15:20:00 +0100

    lvl2 homeWork right start upd

--- lvl2homeWork/script.js
+++ lvl2homeWork/script.js
@@ -1,48 +1,38 @@
 'use strict';
+function isNum(number) {
+    return (!isNaN(parseFloat(number)) && isFinite(number));
+}
 
+const game = function() {
+    const randomNum =  Math.floor(Math.random() * 100) + 1;
+    console.log(randomNum);
 
-let userAmount =  +prompt('Угадай число от 1 до 100');
-        
-let headFunction = function() {
-    
-    let randomAmount = function(min, max) {
-        min = 1;
-        max = 100;
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min;
-    };
-    let num = randomAmount();
+    return function question() {
+        let userAmount = prompt('Угадай число от 1 до 100');
 
-    let question = function() {
-
-        if ( Number.isNaN(userAmount) === true) {
-            userAmount = +prompt('Введи число!');
-        
-        } else if(userAmount === 0 || userAmount === null) {
-            alert('Игра прервана! Перезагрузите страницу для новой игры!');
-            return;
-    
-        } else if (Number.isNaN(userAmount) === false) {
-            if (userAmount > num) {
-                alert('Загаданное число больше');
-                userAmount =  +prompt('Угадай число от 1 до 100');
-    
-            } else if ( userAmount < num) {
+        if (isNum(userAmount)) {
+            userAmount = +userAmount;
+            
+            if (userAmount < 101 && userAmount > randomNum) {
                 alert('Загаданное число меньше');
-                userAmount =  +prompt('Угадай число от 1 до 100');
-    
-            } else if (userAmount === num) {
+                question();
+            } else if (userAmount < 101 && userAmount < randomNum) {
+                alert('Загаданное число больше');
+                question();
+            } else if (userAmount === randomNum) {
                 alert("Поздравляю, вы угадали цифру! Перезагрузите страницу для новой игры!");
-                return;
+            } else {
+                alert("Вы ввели число больше 100!");
+                question();
             }
+        } else if(userAmount === null) {
+            alert('Вы прервали игру! Перезагрузите страницу для новой игры!');
+        } else {
+            alert('Введите число!');
+            question();
         }
-        return question();
     };
-    console.log(num);
-    return question();
 };
 
-let closeFunc = function() {
-    headFunction();
-}();
+let playGame = game();
+playGame();
