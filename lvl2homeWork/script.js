6d8434f7afe206e4156a9d2f59cbb6dc54db101b
cMeTaHKo <bodia122@gmail.com>
2020-02-05 21:39:26 +0100

    bot updated

--- lvl2homeWork/script.js
+++ lvl2homeWork/script.js
@@ -3,47 +3,58 @@ function isNum(number) {
     return (!isNaN(parseFloat(number)) && isFinite(number));
 }
 
+const quantityOfRepeats = 4;
+
 const game = function() {
-    const randomNum =  Math.floor(Math.random() * 100) + 1;
-    console.log(randomNum);
 
     return function question(count) {
+        const randomNum =  Math.floor(Math.random() * 100) + 1;
 
-        function closingFunc() {
+        
+        function tryingCounter() {
             if(count > 1) {
                 count--;
                 question(count);
-            } else if (count <= 1) {
+            } else if (count === 1) {
+                alert('Вы не угадали число, игра окончена!');
                 return;
             }
         }
-
         let userAmount = prompt('Попробуй угадать число от 1 до 100 в рулетке! Количество попыток ограничено в: ' + count);
 
         if (isNum(userAmount)) {
             userAmount = +userAmount;
 
-            if (userAmount < 101 && userAmount > randomNum) {
-                alert('Загаданное число меньше, осталось попыток: ' + count);
-
-            } else if (userAmount < 101 && userAmount < randomNum) {
-                alert('Загаданное число больше, осталось попыток: ' + count);
-
-            } else if (userAmount === randomNum) {
-                alert("Поздравляю, вы угадали цифру! Перезагрузите страницу для новой игры!");
-            } else {
-                alert('Вы ввели число больше 100! Oсталось попыток: ' + count);
+            if (userAmount <= 100 && userAmount > randomNum ) {
+                alert('Загаданное число рулетки меньше, осталось попыток: ' + (count - 1));
+                tryingCounter();
+            } else if (userAmount <= 100 && userAmount < randomNum ) {
+                alert('Загаданное число рулетки больше, осталось попыток: ' + (count - 1));
+                tryingCounter();
+            } else if (userAmount > 100 || userAmount < 0) {
+                alert('Вы ввели число больше 100! Oсталось попыток: ' + (count - 1));
+                tryingCounter();
+            } else if (count === 0) {
+                alert('Вы не угадали число, игра окончена!');
+                return false;
                 
-            }
+            } else {
+                let repeat = confirm("Поздравляю, вы угадали цифру! Хотели бы сыграть ещё ?!");
+                if (repeat === true) {
+                    question(quantityOfRepeats);
+                } else if (repeat === false) {
+                    return;
+                }
+            } 
         } else if(userAmount === null) {
             alert('Вы прервали игру! Перезагрузите страницу для новой игры!');
+            return;
         } else {
-            alert('Введите число!');
-            question();
+            alert('Введите число! ' + ' Oсталось попыток: ' + (count - 1));
+            tryingCounter();
         }
-        closingFunc();
     };
 };
 
 let playGame = game();
-playGame(10);
+playGame(quantityOfRepeats);
